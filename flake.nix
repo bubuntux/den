beee52bf0c20e5e159b4ae21dfe38ff996059066
Julio Gutierrez <413330+bubuntux@users.noreply.github.com>
2026-01-28 16:17:37 -0600

    test

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 # Use `nix run .#write-flake` to regenerate it.
 {
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+     outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     flake-file.url = "github:vic/flake-file";
