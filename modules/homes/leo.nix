db07c0ad7c8d4b91930a3e3ecf012a5d3a43bcd3
Julio Gutierrez <413330+bubuntux@users.noreply.github.com>
2026-01-31 18:25:01 -0600

    adding some comments

--- modules/homes/leo.nix
+++ modules/homes/leo.nix
@@ -1,1 +1,9 @@
-{ }
+{ inputs, self, ... }:
+{
+
+  # flake.homeConfigurations.leo = inputs.home-manager.lib.homeManagerConfiguration {
+  #   modules = [
+  #     self.homeModules.base
+  #   ];
+  # };
+}
