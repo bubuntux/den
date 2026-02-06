408ca0ea6a765856387bfa5e8f3ab08899ef779a
Julio Gutierrez <413330+bubuntux@users.noreply.github.com>
2026-02-06 10:15:45 -0600

    move hardware

--- modules/hosts/katara.nix
+++ modules/hosts/katara.nix
@@ -10,10 +10,13 @@
       profile-laptop
       profile-wife
       (
-        { pkgs, ... }:
         {
-          imports = [ ./_katara-hardware-configuration.nix ];
-
+          config,
+          lib,
+          modulesPath,
+          ...
+        }:
+        {
           networking.hostName = "katara";
           system.stateVersion = "25.11";
 
@@ -23,6 +26,40 @@
           # Install firefox.
           programs.firefox.enable = true;
 
+          imports = [
+            (modulesPath + "/installer/scan/not-detected.nix")
+          ];
+
+          boot.initrd.availableKernelModules = [
+            "nvme"
+            "xhci_pci"
+            "usb_storage"
+            "sd_mod"
+          ];
+          boot.initrd.kernelModules = [ ];
+          boot.kernelModules = [ "kvm-amd" ];
+          boot.extraModulePackages = [ ];
+
+          fileSystems."/" = {
+            device = "/dev/disk/by-uuid/1f24fa25-4b1e-4433-a643-0a585c1a5134";
+            fsType = "ext4";
+          };
+
+          fileSystems."/boot" = {
+            device = "/dev/disk/by-uuid/55CA-E94B";
+            fsType = "vfat";
+            options = [
+              "fmask=0077"
+              "dmask=0077"
+            ];
+          };
+
+          swapDevices = [
+            { device = "/dev/disk/by-uuid/e6ec01aa-d7a0-4623-96fe-a6d9606dd1fc"; }
+          ];
+
+          nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+          hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
         }
       )
     ];
