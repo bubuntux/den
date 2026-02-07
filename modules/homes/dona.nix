e1ffba188019d36ce4ae41d4e2d907135c2b713e
Julio Gutierrez <413330+bubuntux@users.noreply.github.com>
2026-02-06 19:40:06 -0600

    home manager

--- modules/homes/dona.nix
+++ modules/homes/dona.nix
@@ -1,6 +1,13 @@
 { inputs, self, ... }:
 {
+  flake.homeModules.user-dona = {
+    imports = [ self.homeModules.profile-base ];
+    home.stateVersion = "24.05";
+  };
+
   flake.nixosModules.home-dona = {
+    imports = [ inputs.home-manager.nixosModules.home-manager ];
+
     services.displayManager.autoLogin.user = "dona";
     users.users.dona = {
       isNormalUser = true;
@@ -10,12 +17,22 @@
         "wheel"
       ];
     };
+
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.users.dona = {
+      imports = [ self.homeModules.user-dona ];
+    };
   };
 
   flake.homeConfigurations.dona = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     modules = [
-      self.homeModules.profile-base
+      self.homeModules.user-dona
+      {
+        home.username = "dona";
+        home.homeDirectory = "/home/dona";
+      }
     ];
   };
-
 }
